d7a827b5654bed568dd83cfe1998e94f0d6f6c7c
VijaySolanki <37324125+vijaysolankii@users.noreply.github.com>
2019-01-25 22:39:10 +0530

    Update forms.py

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .models import (
     Profile
 )
-
+#basic form
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=50,
